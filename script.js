da07f9c8bce0ac667d5a39d48f42637b3edc9a8c
Ermiyas H <Ermi@Ermiyass-MacBook-Pro-2.local>
2023-09-20 23:49:56 -0700

    dark mode added

--- script.js
+++ script.js
@@ -11,3 +11,24 @@ function toggleMenu() {
   window.addEventListener("load", () => {
     lastName.style.animationName = "slide";
   });
+
+  // Dark mode
+  const toggleButton = document.querySelector("#dark-mode-toggle-button");
+  const body = document.body;
+
+
+  function toggleDarkMode() {
+    console.log("Toggle Dark Mode clicked");
+    body.classList.toggle("dark-mode");
+    const isDarkMode = body.classList.contains("dark-mode");
+    localStorage.setItem("theme", isDarkMode ? "dark-mode" : "light-mode");
+  }
+
+  // Add a click event listener to the button
+  toggleButton.addEventListener("click", toggleDarkMode);
+
+  // Check local storage for the current theme
+  const currentTheme = localStorage.getItem("theme");
+  if (currentTheme) {
+    body.classList.add(currentTheme);
+  }
